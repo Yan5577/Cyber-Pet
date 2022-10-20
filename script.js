f32b17fcd93452937dcd00fbf6e6c24be4a0f705
Ian Williamson <ian@4of.uk>
2022-10-20 16:45:01 +0100

    index animal now selectable

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
+
+//HTML elements
 const overlay = document.querySelector('.overlay');
+const animals = document.querySelectorAll('.animals')
 
 
 
+
+
+
+//Functions
+
+const clearAnimalSelection = ()=>{
+    for (let i=0; i<animals.length; i++){
+       animals[i].classList.remove('selected');
+    }
+}
+
+const selectAnimal =(args)=>{
+    clearAnimalSelection();
+    args.currentTarget.classList.add('selected')
+}
+
+
+//Events
 document.addEventListener('click', (e) =>{
     overlay.classList.add('hide')
 });
+
+for (let i=0; i<animals.length; i++){
+    animals[i].addEventListener('click', (event)=> selectAnimal(event));
+}
